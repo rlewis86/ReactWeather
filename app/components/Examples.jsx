cd2c8aef5069567aaa3ce12eadc6e1a5de7af493
Robert <rlewis@highnotice.com>
2017-03-29 11:19:19 -0400

    Examples page styling

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,22 @@
 var React = require('react');
+var { Link } = require('react-router');
 
 var Examples = (props) => {
     return (
         <div>
-            <h3>Examples</h3>
-            <p>Welcome to the examples page!</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out</p>
+            <ol>
+                <li>
+                    <Link to="/?location=Dover,OH'">Dover, OH</Link>
+                </li>
+                <li>
+                    <Link to="/?location=Cleveland,OH'">Cleveland, OH</Link>
+                </li>
+                <li>
+                    <Link to="/?location=Columbus,OH'">Columbus, OH</Link>
+                </li>
+            </ol>
         </div>
     );
 }
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -3,11 +3,15 @@ var Nav = require('Nav');
 
 var Main = (props) => {
     return (
-            <div>
-                <Nav />
-                {props.children}
+        <div>
+            <Nav />
+            <div className="Row">
+                <div className="columns medium-6 large-4 small-centered">
+                    {props.children}
+                </div>
             </div>
-        );
+        </div>
+    );
 }
 
 module.exports = Main;
